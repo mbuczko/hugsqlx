82210626e5645d3cacb7d2c04c34b3e1a897cf40
Michał Buczko <michal.buczko@gmail.com>
2022-10-29 01:26:31 +0200

    add some basic examples

---
+++ examples/resources/queries.sql
@@ -0,0 +1,11 @@
+-- :name add_user
+-- :doc Creates new user record
+INSERT INTO users(email, name, picture)
+VALUES(?, ?, ?)
+RETURNING user_id
+
+-- :name fetch_user_by_email :as :?
+-- :doc Returns user based on its identifier
+SELECT user_id, email, name, picture
+  FROM users
+ WHERE email = ?
---
+++ examples/src/main.rs
@@ -0,0 +1,57 @@
+use std::env;
+
+use futures::stream::FuturesOrdered;
+use futures::StreamExt;
+use hugsqlx::{params, HugSqlx};
+use sqlx::SqlitePool;
+use uuid::Uuid;
+
+#[derive(HugSqlx)]
+#[queries = "examples/resources/queries.sql"]
+struct Queries {}
+
+#[derive(Debug, sqlx::FromRow)]
+struct User {
+    user_id: i32,
+    email: String,
+    name: String,
+    picture: String,
+}
+
+fn generate_email() -> String {
+    format!("janko-{}@foo.com", Uuid::new_v4())
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let pool = SqlitePool::connect(&env::var("DATABASE_URL")?).await?;
+    let email = generate_email();
+
+    // insert single user and fetch data back from DB
+    let result = Queries::add_user(
+        &pool,
+        params!(&email, "Janko Muzykant", "http://my.profile.image.com"),
+    )
+    .await?;
+    if let Some(user) = Queries::fetch_user_by_email::<_, User>(&pool, params!(email)).await? {
+        println!("user {:?}", user);
+    } else {
+        eprintln!("Somefink went really wrong. Insertion result: {:?}", result);
+    }
+
+    // ok, let's insert few more users
+    let results = vec![
+        ("Alice", "http://profile-1.com"),
+        ("Bob", "http://profile-2.com"),
+        ("Mallory", "http://profile-3.com"),
+    ]
+    .iter()
+    .map(|(name, picture)| Queries::add_user(&pool, params!(generate_email(), name, picture)))
+    .collect::<FuturesOrdered<_>>()
+    .collect::<Vec<_>>()
+    .await;
+
+    assert_eq!(results.len(), 3);
+
+    Ok(())
+}
